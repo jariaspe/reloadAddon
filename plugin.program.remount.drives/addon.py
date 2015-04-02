e14dfc6400b6d182a0fdaa7168d217b37f5ffe93
jariaspe <javierarias@gmx.es>
2015-04-02 10:26:47 +0200

    Clean comments and add internationalization

--- plugin.program.remount.drives/addon.py
+++ plugin.program.remount.drives/addon.py
@@ -1,27 +1,27 @@
 import xbmcaddon
 import xbmcgui
-import xbmc
 import os
 
 
 addon 		= xbmcaddon.Addon()
-addonname 	= addon.getAddonInfo('name')
+
+dialogTitle = addon.getLocalizedString(32000)
+dialogText = addon.getLocalizedString(32001)
+okText = addon.getLocalizedString(32002)
+
+dialog = xbmcgui.Dialog()
 
 class Remount():
 	def lauch(self):
-		xbmc.log("init launch")
 		os.system("mount -o loop /dev/sda /media/DiscoRasp/")
+		dialog.ok(dialogTitle, okText)
 
-xbmc.log("after Remount definition")
+
 remount = Remount()
-xbmc.log("after remount object instantation")
-dialog = xbmcgui.Dialog()
-xbmc.log("after dialog instantation")
 
-if dialog.yesno("addonname", "Remount drives"):
-	xbmc.log("if yesno clicked")
+
+if dialog.yesno(dialogTitle, dialogText):
 	remount.lauch()
 
 del dialog
 del remount
-xbmc.log("end of file")
